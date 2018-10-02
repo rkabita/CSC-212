4a943b63608940591d2fc8bddc583a3c99502045
rkabita <36805923+rkabita@users.noreply.github.com>
2018-10-02 17:36:58 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/* int main()
+{
+    // You can run your own test code here as you develop your class to check that it works as you expected.
+    // Compile `sequence_test.cpp` and `sequence_exam.cpp` as separate executables to test your class with that code.
+    // They both contain a main function, meaning you cannot compile them with this `main.cpp` file.
+    std::cout << "Hello, World!" << std::endl;
+    return EXIT_SUCCESS;
+} */
